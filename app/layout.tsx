f2bab5ad782f84b46521cc145a40da9ce84dd55e
Kyle Esterhuizen <kyle@knetcode.com>
2022-11-06 21:51:44 +0200

    env force

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
-export default function RootLayout({ children }: {
+export default function RootLayout({
+  children,
+}: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ backgroundColor: "palegoldenrod" }}>{children}</body>
     </html>
   );
 }
